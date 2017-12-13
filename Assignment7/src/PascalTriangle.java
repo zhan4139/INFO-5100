4e399afeebcc90560a4da487a35ccf61ef683680
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-12 17:30:21 -0800

    score update

--- Assignment7/src/PascalTriangle.java
+++ Assignment7/src/PascalTriangle.java
@@ -1,6 +1,6 @@
 
 public class PascalTriangle {
-	public static void main(String[] args) {
+	public static void main(String[] args) { // score 2
 		printPascalTriangle(7);
 	}
 	//Print first n lines of Pascal Triangle
